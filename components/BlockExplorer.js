75c272e5e56c667417cdd1500f151c1c0cb1dac9
luke <luke@prescryptive.com>
2022-02-28 10:43:21 -0800

    got past the cors issue

--- components/BlockExplorer.js
+++ components/BlockExplorer.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 function BlockExplorer() {
     const [blocks, setBlocks] = useState([]);
     useEffect(() => {
-        const provider = new Provider(["http://localhost:8081"]);
+        const provider = new Provider(["http://localhost:8081/jsonrpc"]);
         provider.getBlocks(1,1)
             .then((res) => setBlocks(res))
             .catch((err) => window.alert(err));
@@ -12,7 +12,9 @@ function BlockExplorer() {
 
     return (
         <div>
-            {blocks}
+            {blocks.map((block) => (
+                <span key={block.block_id}>{block.block_id}</span>
+            ))}
         </div>
     );
 }
